f3750220ba63ec903b0a7265a7f22845078ac321
sumniikit <14shepelyonok88@gmail.com>
2021-10-19 20:10:05 +0300

    update login form

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,19 +1,21 @@
 import { Button, Form, Input } from "antd"
 import axios from "axios"
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import "./Components.css"
-import { Link } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 
 const LoginForm = () => {
     const[loginInput, setLoginInput] = useState(null);
     const[passwordInput, setPasswordInput] = useState(null);
+    const { push } = useHistory();
 
-    const onFinish = async(values) => {
+    const onFinish = async() => {
         const response = await axios.post("http://localhost:51693/api/user", {userLogin: loginInput, userPassword: passwordInput});
         console.log('response', response);
-        if (response.status === 200)
+        if (response)
         {
-            <Link to="/admin"></Link>
+            push("/admin");
         }
     }
 
@@ -31,48 +33,50 @@ const LoginForm = () => {
 
     return (
         <div className="LoginForm">
-            <Form name="basic"
-            labelCol={{span: 8}}
-            wrapperCol={{span: 16}}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off">
-                <Form.Item 
-                label="Username"
-                name="username"
-                rules={
-                    [
+            <Route>
+                <Form name="basic"
+                labelCol={{span: 8}}
+                wrapperCol={{span: 16}}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off">
+                    <Form.Item 
+                    label="Username"
+                    name="username"
+                    rules={
+                        [
+                            {
+                                required: true,
+                                message: "Please input your username!"
+                            }
+                        ]
+                    }>
+                        <Input onChange={onLoginValueChange}
+                            value={loginInput} />
+                    </Form.Item>
+                    <Form.Item 
+                    label="Password"
+                    name="password"
+                    rules = {[
                         {
                             required: true,
-                            message: "Please input your username!"
+                            message: "Please input your password!"
                         }
-                    ]
-                }>
-                    <Input onChange={onLoginValueChange}
-                           value={loginInput} />
-                </Form.Item>
-                <Form.Item 
-                label="Password"
-                name="password"
-                rules = {[
-                    {
-                        required: true,
-                        message: "Please input your password!"
-                    }
-                ]}>
-                    <Input.Password onChange={onPasswordValueChange}
-                                    value={setPasswordInput} />
-                </Form.Item>
-                <Form.Item 
-                wrapperCol={{
-                    offeset: 8,
-                    span: 16
-                }}>
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                </Form.Item>
-            </Form>
+                    ]}>
+                        <Input.Password onChange={onPasswordValueChange}
+                                        value={setPasswordInput} />
+                    </Form.Item>
+                    <Form.Item 
+                    wrapperCol={{
+                        offeset: 8,
+                        span: 16
+                    }}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Route>
         </div>
     );
 };
